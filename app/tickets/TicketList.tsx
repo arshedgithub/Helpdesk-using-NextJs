6f243fdf0070b5c4e021207ea96e7f576d264f20
arshedgithub <arshedahmedmail@gmail.com>
2024-08-24 10:08:52 +0530

    add click routing

--- app/tickets/TicketList.tsx
+++ app/tickets/TicketList.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 async function getTickets(): Promise<Ticket[]> {
     const res = await fetch('http://localhost:4000/tickets', {
         next: {
@@ -22,10 +24,12 @@ export default async function TicketList() {
     return (
         <>
             {tickets.map(ticket => (
-                <div key={ticket.id} className="card">
-                    <h3>{ticket.title}</h3>
-                    <p>{ticket.body.slice(0, 200)}...</p>
-                    <div className={`pill ${ticket.priority}`}>{ticket.priority} priority</div>
+                <div key={ticket.id} className="card my-5">
+                    <Link href={`/tickets/${ticket.id}`}>
+                        <h3>{ticket.title}</h3>
+                        <p>{ticket.body.slice(0, 200)}...</p>
+                        <div className={`pill ${ticket.priority}`}>{ticket.priority} priority</div>
+                    </Link>
                 </div>
             ))}
             {tickets.length == 0 && (<p className="text-center">There are no open Tickets...</p>)}
