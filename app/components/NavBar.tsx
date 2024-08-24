f8faba69656dd231da0b57d43f8ee111ed549552
arshedgithub <arshedahmedmail@gmail.com>
2024-08-24 08:07:57 +0530

    add image to nav

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,10 +1,18 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import logo from './dojo-logo.png'
 
 const NavBar = () => {
   return (
     <nav>
+      <Image
+        src={logo}
+        alt='logo'
+        width={70}
+        quality={100}
+        placeholder='blur'
+      ></Image>
       <h1>My Helpdesk</h1>
       <Link href="/">Dashboard</Link>
       <Link href="/tickets">Tickets</Link>
