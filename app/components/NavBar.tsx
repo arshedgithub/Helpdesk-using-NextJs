f6398263cc54f05fef47ee76532a47defc56fce1
arshedgithub <arshedahmedmail@gmail.com>
2024-08-24 07:58:04 +0530

    add main page

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,21 +1,13 @@
+import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
 const NavBar = () => {
   return (
-    <nav className="flex justify-between px-4 py-2 bg-gray-800 text-white">
-      <h1 className="font-bold text-2xl">Ticket Save</h1>
-      <ul className="flex gap-4">
-        <li>
-          <Link href="#">Home</Link>
-        </li>
-        <li>
-          <Link href="#">About</Link>
-        </li>
-        <li>
-          <Link href="#">Contact</Link>
-        </li>
-      </ul>
+    <nav>
+      <h1>My Helpdesk</h1>
+      <Link href="/">Dashboard</Link>
+      <Link href="/tickets">Tickets</Link>
     </nav>
   )
 }
